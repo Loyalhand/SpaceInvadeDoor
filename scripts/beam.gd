bf5c70a9d896d343be76a16e24c8b45710cc8915
Matt Pahayo <mpahayo@gmail.com>
2025-03-15 02:54:16 -0500

    small enemy can now shoot

--- scripts/beam.gd
+++ scripts/beam.gd
@@ -4,8 +4,7 @@ const SPEED = 2000
 var damage = 10
 
 func _physics_process(delta):
-	position.y -= SPEED * delta
-
+	position += (Vector2.RIGHT * SPEED).rotated(rotation) * delta
 
 func _on_visible_on_screen_enabler_2d_screen_exited():
 	queue_free()
--- scripts/small_enemy.gd
+++ scripts/small_enemy.gd
@@ -1,16 +1,25 @@
 extends Sprite2D
 
 var health: int = 100
-
+var can_shoot: bool = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
 	pass
 
+func shoot():
+	const BEAM = preload("res://scenes/beam.tscn")
+	var beam_instance = BEAM.instantiate()
+	beam_instance.rotation_degrees = rotation_degrees - 90
+	beam_instance.global_position = global_position
+	add_sibling(beam_instance)
 
 func _on_hurt_box_health_depleted():
 	print("dead")
+
+func _on_fire_rate_timeout():
+	$FireRate.wait_time = randf_range(0,5)
+	shoot()
